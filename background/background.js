13c8f86ceada1e81558ca05c0fb316f1f23f9239
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-09-11 15:45:10 +0200

    Initialize default settings via 'onInstalled' event listener

--- background/background.js
+++ background/background.js
@@ -1,3 +1,17 @@
+import {saveSettings} from "../shared/shared_functions.js";
+
+// Add an event listener for the 'onInstalled' event, which means it will run when the extension when it will be first installed
+browser.runtime.onInstalled.addListener(async (details) => {
+  if (details.reason === "install") {
+    await saveSettings({
+      sensitiveSearch: false,
+      whitelistFirefoxReservedTabs: false
+    });
+  }
+
+  return;
+});
+
 // Listen for incoming messages from the extension's UI "Close Tabs" button pressed
 browser.runtime.onMessage.addListener(async (message) => {
   if (await message.action !== "closeTabs") {
